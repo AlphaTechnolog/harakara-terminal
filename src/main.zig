e757ce520c18fb56bb541f9dbaf1598636d77007
Gabriel Guerra <gfranklings@gmail.com>
2024-08-10 13:35:59 -0400

    chore: using the right utils for type casting in main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
 const c = @import("./lib/c.zig");
+const utils = @import("./lib/utils.zig");
 const Application = @import("./lib/application.zig");
 const Terminal = @import("./terminal/init.zig");
 
 fn activate(_: *c.GtkApplication, user_data: c.gpointer) void {
-    const app = @as(*Application, @constCast(@alignCast(@ptrCast(user_data))));
-
+    const app = utils.castFromGPointer(Application, user_data);
     var terminal = Terminal.init(std.heap.page_allocator, app) catch unreachable;
     terminal.setup();
     terminal.window.asWidget().showAll();
@@ -18,8 +18,8 @@ pub fn main() u8 {
 
     _ = app.connect(
         "activate",
-        @as(c.GCallback, @constCast(@ptrCast(@alignCast(&activate)))),
-        @as(c.gpointer, @constCast(@ptrCast(&app))),
+        utils.castGCallback(activate),
+        utils.castGPointer(&app),
     );
 
     return @intCast(app.toGApplication().run(0, null));
